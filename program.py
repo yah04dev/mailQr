e2d90742616cc65ee6a531671ec36f49fbc3e779
yah04dev <46074913+yah04dev@users.noreply.github.com>
2023-02-26 18:07:33 +0100

    Update program.py

--- program.py
+++ program.py
@@ -9,19 +9,16 @@ import tkinter as tk
   
 root=tk.Tk()
  
-# setting the windows size
+
 root.geometry("300x100")
 root.title('email to qr')
 root.iconbitmap('search.ico')  
-# declaring string variable
-# for storing name and password
+
 idt=tk.StringVar()
 
  
   
-# defining a function that will
-# get the name and password and
-# print them on the screen
+
 def submit():
     def printqrc(email,passd):
         file_path = "output.png"
@@ -34,23 +31,22 @@ def submit():
         print("QR code generated successfully.")
 
 
-# Open a connection to the database file
     conn = sqlite3.connect("emails.db")
 
-# Create a cursor object to execute SQL statements
+
     cur = conn.cursor()
 
-# Get input ID from user
+
     id=idt.get()
    
 
-# Execute a SELECT statement to retrieve the name and date of birth based on the input ID
+
     cur.execute("SELECT email, passd FROM mailiste WHERE id=?", (id,))
     result = cur.fetchone()
 
-# Check if result is not None
+
     if result is not None:
-    # Print the name and date of birth
+    
             email, passd = result
             printqrc(email,passd)
     else:
@@ -60,30 +56,18 @@ def submit():
     conn.close()
 
      
-    
-     
-     
-# creating a label for
-# name using widget Label
+
 name_label = tk.Label(root, text = 'ID: ', font=('calibre',10, 'bold'))
   
-# creating a entry for input
-# name using widget Entry
+
 name_entry = tk.Entry(root,textvariable = idt, font=('calibre',10,'normal'))
   
-
-# creating a button using the widget
-# Button that will call the submit function
 sub_btn=tk.Button(root,text = 'Print', command = submit)
-  
-# placing the label and entry in
-# the required position using grid
-# method
+
 name_label.grid(row=0,column=0)
 name_entry.grid(row=0,column=1)
 
 sub_btn.grid(row=2,column=1)
   
-# performing an infinite loop
-# for the window to display
+
 root.mainloop()
